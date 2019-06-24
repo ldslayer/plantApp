b670135917832f0af1c7fb964458d9dabd624004
ldslayer <bibincito@gmail.com>
2019-06-24 13:07:27 -0400

    mocks 16:40

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # plantApp
 React Native Plant App
 
-Siguiendo el tutorial: https://youtu.be/gyiwFcrVRCM
+Siguiendo el [tutorial:](https://youtu.be/gyiwFcrVRCM)
+
+
+1. Creamos el proyecto: **react-native init plantApp**
+    - Alternativa sin android: **expo init plantApp**
+1. Agregamos el paquete para sliders: **yarn add react-native-slider**    
+1. Agregamos los paquetes de navegación: **yarn add react-navigation**
+1. Agregamos paquetes para iconos **yarn add react-native-vector-icons**
---
+++ constants/mocks.js
@@ -0,0 +1,52 @@
+const categories = [
+    {
+        id: 'plants',
+        name: 'Plants',
+        tags: ['products', 'inspirations', 'shop'],
+        count: 147,
+        image: require('../assets/images/plants.png')
+    },
+    {
+        id: 'seeds',
+        name: 'Seeds',
+        tags: ['products', 'inspirations', 'shop'],
+        count: 68,
+        image: require('../assets/images/seeds.png')
+    },
+    {
+        id: 'flowers',
+        name: 'Flowers',
+        tags: ['products', 'inspirations',],
+        count: 200,
+        image: require('../assets/images/flowers.png')
+    },
+    {
+        id: 'sprayers',
+        name: 'Sprayers',
+        tags: ['products', 'shop'],
+        count: 56,
+        image: require('../assets/images/sprayers.png')
+    },
+    {
+        id: 'pots',
+        name: 'Pots',
+        tags: ['products', 'inspirations', 'shop'],
+        count: 56,
+        image: require('../assets/images/pots.png')
+    },
+    {
+        id: 'fertilizers',
+        name: 'Fertilizers',
+        tags: ['products', 'shop'],
+        count: 83,
+        image: require('../assets/images/fertilizers.png')
+    },
+
+]
+
+const products = []
+
+const explore = [
+    // images
+]
+
